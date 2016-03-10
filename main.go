975f887010b9a4d389c8a9d11a529f2aaf6191ea
ThyLeader <colin@cadler.co>
2016-03-10 08:34:59 -0500

    Split into two files, README to come soon

--- main.go
+++ main.go
@@ -6,42 +6,42 @@ import (
         "github.com/faith/color"
 		"math/rand"
 		"time"
+        "strings"
 		//"net/http"
 )
 
 func main() {
+    //d := color.New(color.FgRed, color.Bold)
     mode := printWelcome()
-    dice := rollDice(6)
-    fmt.Println(dice, mode)
-}
-
-func printWelcome() int{
-    mode := 0
-    d := color.New(color.FgRed, color.Bold)
-    d.Println("Welcome to Farkle, a fairly convoluted dice game that may be fun.")
-    d.Println("Would you like to play against ai or a friend? (1 for by yourself, 2 for ai, 3 for friends):")
-    d.Println("Hint: you can currently only play by yourself. Deal with it")
-    for mode == 0 {
-        if _, err := fmt.Scanln(&mode); err != nil {
-            log.Fatal(err)
-        }
-        if mode == 1 {
-            break
-        } else {
-            d.Println("Please choose 1")
-            mode = 0
-        }
+    if mode == 1{
+        singlePlayer()
     }
-
-    return mode
 }
 
-func rollDice(len int) []int {
+func rollDice(len int) ([]int, int) {
     toReturn := make([]int, len) 
     for i, _ := range toReturn {
         r := rand.New(rand.NewSource(time.Now().UTC().UnixNano()))
         d := r.Intn(6) + 1
         toReturn[i] = d
     }
-    return toReturn
+    return toReturn, len
+}
+
+func singlePlayer() {
+    var mode string
+    d := color.New(color.FgRed, color.Bold)
+    d.Println("Singleplayer initiated")
+    d.Println("Type \"Rules\" to see the rules, otherwise type anything else")
+    if _, err := fmt.Scanln(&mode); err != nil {
+        log.Fatal(err)
+        d.Println(mode)
+    }
+    if strings.ToLower(mode) == "rules" {
+        printRules()
+        printScoring()
+    }
+    dice, amt := rollDice(6)
+    d.Println("\nYou rolled", amt, "dice")
+    d.Println(dice)
 }
---
+++ supportingFunctions.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+        "fmt"
+	    "log" 
+        "github.com/faith/color"
+		"time"
+		//"net/http"
+)
+
+func printWelcome() int{
+    mode := 0
+    d := color.New(color.FgRed, color.Bold)
+    d.Println("Welcome to Farkle, a fairly convoluted dice game that may be fun.")
+    d.Println("Would you like to play against ai or a friend? (1 for by yourself, 2 for ai, 3 for friends):")
+    d.Println("Hint: you can currently only play by yourself. Deal with it")
+    for mode == 0 {
+        if _, err := fmt.Scanln(&mode); err != nil {
+            log.Fatal(err)
+        }
+        if mode == 1 {
+            break
+        } else {
+            d.Println("Please choose 1")
+            mode = 0
+        }
+    }
+
+    return mode
+}
+
+func printRules() {
+    var isDone string
+    d := color.New(color.FgRed, color.Bold)
+    ruleArray := []string{"At the beginning of each turn, the player throws all the dice at once from a cup.", "After each throw, one or more scoring dice must be set aside.", "The player may then either end their turn and bank the score accumulated so far, or continue to throw the remaining dice.", "If the player has scored all six dice, they have \"hot dice\" and may continue their turn with a new throw of all six dice, adding to the score they have already accumulated. There is no limit to the number of \"hot dice\" a player may roll in one turn.", "If none of the dice score in any given throw, the player has \"farkled\" and all points for that turn are lost.", "At the end of the player's turn, the dice are handed to the next player in succession (usually in clockwise rotation), and they have their turn."}
+    
+    for _, e := range ruleArray {
+        d.Println(e)
+        time.Sleep(1000 * time.Millisecond)
+    }
+    d.Println("\nType anything when you have finished reading the rules to continue")
+    if _, err := fmt.Scanln(&isDone); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func printScoring() {
+    //d := color.New(color.FgRed, color.Bold)
+    fmt.Println(`
+==============================
+| Dice combination |  Score  |
+|    Each 1        |  100    |
+|    Each 5        |  50     |
+|    Three 1s      |  1000   |
+|    Three 2s      |  200    |
+|    Three 3s      |  300    |
+|    Three 4s      |  400    |
+|    Three 5s      |  500    | 
+|    Three 6s      |  600    |
+==============================`)
+}
